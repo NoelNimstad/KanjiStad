c42ad3a909f601038ea9d16c0861375e73c64320
basyl <69351096+thebasyl@users.noreply.github.com>
2023-11-26 11:31:31 +0100

    basic functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// SCORE
+let correct = 0;
+let total = 0;
+let correctBool = false;
+const scoreLabel = document.getElementById("score");
+
 const range = document.getElementById("range");
 const levelLabel = document.getElementById("level");
 
@@ -10,9 +16,75 @@ range.addEventListener("input", () =>
     levelLabel.innerHTML = `・${ levels }ヶレベル`;
 });
 
-async function Main()
+const startButton = document.getElementById("start");
+
+let filteredVocabulary = [];
+let currentItem = {};
+let currentCharacter = "";
+startButton.addEventListener("click", () => 
 {
-    
+    filteredVocabulary = vocabulary.filter(item => item.level <= levels);
+    Prompt();
+});
+
+let prompt = "";
+const promptLabel = document.getElementById("prompt");
+function Prompt()
+{
+    correctBool = false;
+
+    KanjiCanvas.erase("canvas");
+    currentItem = filteredVocabulary[Math.floor(Math.random() * filteredVocabulary.length)];
+
+    prompt = currentItem.character;
+    currentCharacter = currentItem.character.match(/[\u4e00-\u9faf]/g);
+    currentCharacter = currentCharacter[Math.floor(Math.random() * currentCharacter.length)]
+
+    prompt = prompt.replace(currentCharacter, "○");
+    promptLabel.innerHTML = prompt + "《" + currentItem.kana + "》<br/>" + currentItem.meaning;
 }
 
-Main();
+const undoButton = document.getElementById("undo");
+undoButton.addEventListener("click", () => 
+{
+    KanjiCanvas.deleteLast("canvas");
+});
+
+const restartButton = document.getElementById("restart");
+restartButton.addEventListener("click", () => 
+{
+    KanjiCanvas.erase("canvas");
+});
+
+const correctButton = document.getElementById("correct");
+correctButton.addEventListener("click", () => 
+{
+    const results = KanjiCanvas.recognize("canvas");
+
+    prompt = prompt.replace("○", currentCharacter);
+    promptLabel.innerHTML = prompt;
+
+    total++;
+
+    if(results.includes(results))
+    {
+        correct++;
+        correctBool = true;
+    }
+
+    scoreLabel.innerHTML = `${ correct }/${ total } : ${ correct / total * 100 }%`;
+});
+
+const nextButton = document.getElementById("next");
+nextButton.addEventListener("click", () => 
+{
+    if(!correctBool)
+    {
+        total++;
+        scoreLabel.innerHTML = `${ correct }/${ total } : ${ correct / total * 100 }%`;
+    }
+
+    correctBool = false;
+
+    Prompt();
+});
