49dc5725a01a6d23f9482dd541f4152b97d8d412
basyl <69351096+thebasyl@users.noreply.github.com>
2023-11-26 10:49:24 +0100

    base finished

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const range = document.getElementById("range");
+const levelLabel = document.getElementById("level");
+
 KanjiCanvas.init("canvas");
+
+let levels = 10;
+range.addEventListener("input", () => 
+{
+    levels = range.value;
+    levelLabel.innerHTML = `・${ levels }ヶレベル`;
+});
+
+async function Main()
+{
+    
+}
+
+Main();
